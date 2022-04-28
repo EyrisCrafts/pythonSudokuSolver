2b7dec65000fce8be558ba25a2b1bb89e61ff56c
waleed <wkhan@bridgelinxpk.com>
2022-04-28 10:28:35 +0500

    fixed code and updated to python3

--- solver.py
+++ solver.py
@@ -1,17 +1,17 @@
 import itertools
-## Empty Board will be updated
+
 board = []
 
 def load_board():
 	f = open("puzzle.txt","r")	#The sudoku problem should be in 'puzzle.txt'
 	for line in f:
-		board.append(map(int,list(line.strip())))
+		board.append(list(map(int,list(line.strip()))))
 	f.close()
 	return board
 
 def printBoard():
 	for line in board:
-		print ' '.join(map(str,line))
+		print (' '.join(map(str,line)))
 
 def row(hLine):
 	return [i for i in board[hLine]]
@@ -19,28 +19,12 @@ def row(hLine):
 def column(vLine):
 	return [line[vLine] for line in board]
 
+# Calculate and return the 3x3 box
 def box(n):
-	return [line[(n%3)*3:(n%3)*3+3] for line in board[(n/3)*3:(n/3)*3+3]]
+	return [line[int((n%3)*3):int((n%3)*3+3)] for line in board[int((n//3)*3) : int(((n//3)*3)+3)]]
 
-#working on Solving the box
-def solveBox(n,number):
-	if number in list(itertools.chain.from_iterable(box(n))):
-		return
-	marker =  list(itertools.chain.from_iterable(box(n)))
-	
-	
-	for i in range(3):
-		for j in range(3):
-			if board[(n/3)*3+i][j+(n%3)*3] != 0:
-				continue
-			for l in  box(n):
-				if number in l:
-					continue
-			if number in row((n/3)*3+i):
-				continue
-			if number in column((n%3)*3+j):
-				continue
-			board[(n/3)*3+i][j+(n%3)*3] = number
+def isSolved():
+	return not (0 in itertools.chain.from_iterable(board))
 
 def solvehLine(hl,number):
 	if number in row(hl):
@@ -51,7 +35,7 @@ def solvehLine(hl,number):
 			continue
 		if number in column(i):
 			continue
-		b= list(itertools.chain.from_iterable(box((hl/3) * 3  + i%3 -1)))
+		b = list(itertools.chain.from_iterable(box((i//3) + (hl//3)*3)))
 		if number in b:
 			continue
 		marker[i] = -1
@@ -69,7 +53,7 @@ def solvevLine(vl,number):
 			continue
 		if number in row(i):
 			continue
-		b= list(itertools.chain.from_iterable(box((i/3) * 3  + vl%3 -1)))
+		b = list(itertools.chain.from_iterable(box((i//3) * 3  + vl//3)))
 		if number in b:
 			continue
 		marker[i] = -1
@@ -78,11 +62,68 @@ def solvevLine(vl,number):
 	index=marker.index(-1)
 	board[index][vl] = number
 
+def solveBox(n,number):
+	if number in list(itertools.chain.from_iterable(box(n))):
+		return
+	marker = box(n)
+	for i in range(3):
+		for j in range(3):
+			if marker[i][j] != 0:
+				continue
+			if number in row(int(n//3)*3+i):
+				continue
+			if number in column(int(n%3)*3+j):
+				continue
+			
+			board[int(n//3)*3+i][j+int(n%3)*3] = -1
+	if ((list(itertools.chain.from_iterable(board))).count(-1) != 1):
+		cleanBoard()
+	else:
+		index = findNumber(-1)
+		if (index != (-1,-1)):
+			board[index[0]][index[1]] = number
 
+def findNumber(number):
+	for i,line in enumerate(board):
+		for j,boardNumber in enumerate(line):
+			if boardNumber == number:
+				return (i, j)
+	return (-1,-1)
+	
+
+def cleanBoard():
+	for i in range(9):
+		for j in range(9):
+			if (board[i][j] == -1):
+				board[i][j] = 0
 
 board = load_board()
+print("Board loaded\n")
 printBoard()
-print "\n\n"
-solvevLine(4,6)
-printBoard()
+print ("\n\n")
+
+iteration = 0
+
+while(not isSolved()):
+	iteration = iteration + 1
+	print("Doing Iteraction "+str(iteration) + " \n")
+	# Copy the board before attempting solve
+	copyOfBoard = [x[:] for x in board]
+
+	for i in range(9):
+		for number in range(1,10):
+			solvehLine(i,number)
+			solvevLine(i,number)
+			solveBox(i,number)
+	
+	# Check if there is any difference
+	if (list(itertools.chain.from_iterable(copyOfBoard)) == list(itertools.chain.from_iterable(board))):
+		break
+
+if (isSolved()):
+	print("Solved !\n")
+	printBoard()
+else:
+	print("Stuck at Board\n")
+	printBoard()
 
